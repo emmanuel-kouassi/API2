77e9a727e83a94317202203cabd867f8288cb0f7
emmanuel-kouassi <115140578+emmanuel-kouassi@users.noreply.github.com>
2026-01-14 00:37:38 +0100

    n

--- src/main/java/com/example/sae/Models/Session.java
+++ src/main/java/com/example/sae/Models/Session.java
@@ -1,6 +1,7 @@
 package com.example.sae.Models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class Session {
 
     @ManyToOne
     @JoinColumn(name = "id_user") // Nom de la colonne dans votre table SQL
+    @JsonIgnore
     private User user;
 
     @ManyToOne
